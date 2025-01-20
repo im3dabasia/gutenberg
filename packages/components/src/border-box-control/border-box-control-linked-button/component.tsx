ada34e92151d87c29275c36a6ebd8fdac414b9fc
im3dabasia <eshaan.dabasiya@rtcamp.com>
2025-01-20 21:42:10 +0530

    fix: Removed view component

--- packages/components/src/border-box-control/border-box-control-linked-button/component.tsx
+++ packages/components/src/border-box-control/border-box-control-linked-button/component.tsx
@@ -8,7 +8,6 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import Button from '../../button';
-import { View } from '../../view';
 import type { WordPressComponentProps } from '../../context';
 import { contextConnect } from '../../context';
 import { useBorderBoxControlLinkedButton } from './hook';
@@ -24,16 +23,15 @@ const BorderBoxControlLinkedButton = (
 	const label = isLinked ? __( 'Unlink sides' ) : __( 'Link sides' );
 
 	return (
-		<View className={ className }>
-			<Button
-				{ ...buttonProps }
-				size="small"
-				icon={ isLinked ? link : linkOff }
-				iconSize={ 24 }
-				label={ label }
-				ref={ forwardedRef }
-			/>
-		</View>
+		<Button
+			{ ...buttonProps }
+			size="small"
+			icon={ isLinked ? link : linkOff }
+			iconSize={ 24 }
+			label={ label }
+			ref={ forwardedRef }
+			className={ className }
+		/>
 	);
 };
 
